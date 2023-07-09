f02c44c2245a2d575a00d3b275acb05ee99f6c8b
Aaron Maslen <aaron.maslen@gmail.com>
2023-07-09 12:10:48 +1000

    New patch: Add TypeIds from Owlmod assemblies to TypeId cache - should allow json blueprints to use components from (owl)mod assemblies

---
+++ AddTypeIdsFromMods.patch/OwlcatModification.LoadAssemblies_Patch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using HarmonyLib;
+
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.Modding;
+
+namespace WrathPatches
+{
+    [HarmonyPatch(typeof(OwlcatModification), "LoadAssemblies")]
+    internal static class OwlcatModification_LoadAssemblies_Patch
+    {
+        [HarmonyPostfix]
+        static void Postfix(OwlcatModification __instance)
+        {
+            foreach (var assembly in __instance.LoadedAssemblies)
+            {
+                var binder = (GuidClassBinder)Json.Serializer.Binder;
+
+                foreach (var (type, guid) in assembly.GetTypes()
+                    .Select(type => (type, type.GetCustomAttribute<TypeIdAttribute>()?.GuidString))
+                    .Where(t => t.GuidString is not null))
+                {
+                    WrathPatches.Main.Logger.Log($"Adding {type} with TypeId {guid} to binder cache");
+                    binder.AddToCache(type, guid);
+                }
+            }
+        }
+    }
+}
