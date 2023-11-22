6b747db7a318243435f307676ea96e4fc849d40e
Aaron Maslen <aaron.maslen@gmail.com>
2023-11-22 21:53:11 +1100

    Move all patches to the main WrathPatches project and use Harmony's PatchClassProcessor to run them separately

--- WrathPatches/Experimental/BlueprintsCacheInitAddBlueprint.cs
+++ WrathPatches/Experimental/BlueprintsCacheInitAddBlueprint.cs
@@ -11,18 +11,22 @@ using Kingmaker.Blueprints.JsonSystem;
 
 namespace WrathPatches.Experimental
 {
+    [HarmonyPatchCategory("Experimental")]
     [HarmonyPatch(typeof(BlueprintsCache))]
     internal static class BlueprintsCacheInitAddBlueprint
     {
         [HarmonyPatch(nameof(BlueprintsCache.Init))]
+        [HarmonyTranspiler]
         internal static IEnumerable<CodeInstruction> Init_Transpiler(IEnumerable<CodeInstruction> instructions)
         {
+            Main.Logger.Log($"{nameof(BlueprintsCacheInitAddBlueprint)}.{nameof(Init_Transpiler)}");
+
             var (_, instruction) = instructions.Indexed()
                 .First(i => i.item.Calls(
                     AccessTools.Method(typeof(Dictionary<BlueprintGuid, BlueprintsCache.BlueprintCacheEntry>),
                         nameof(Dictionary<BlueprintGuid, BlueprintsCache.BlueprintCacheEntry>.Add))));
 
-            instruction.operand = AccessTools.PropertySetter(typeof(BlueprintsCache), "Item");
+            instruction.operand = AccessTools.PropertySetter(typeof(Dictionary<BlueprintGuid, BlueprintsCache.BlueprintCacheEntry>), "Item");
 
             return instructions;
         }
--- WrathPatches/Main.cs
+++ WrathPatches/Main.cs
@@ -16,49 +16,89 @@ namespace WrathPatches
 
         public static UnityModManager.ModEntry.ModLogger Logger => ModEntry!.Logger;
 
+        static void RunPatches(IEnumerable<(Type, PatchClassProcessor)> typesAndPatches)
+        {
+            foreach (var (t, pc) in typesAndPatches)
+            {
+                try
+                {
+                    Logger.Log($"Running patch class {t.Name}");
+                    pc.Patch();
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error($"Exception in patch class {t.Name}");
+                    Logger.LogException(ex);
+                }
+            }
+        }
+
         static bool Load(UnityModManager.ModEntry modEntry)
         {
             ModEntry = modEntry;
 
             var harmony = new Harmony(modEntry.Info.Id);
 
-            foreach (var assFile in Directory.EnumerateFiles(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "*.patch.dll"))
-            {
-                Assembly? ass = null;
+            var patchClasses = AccessTools.GetTypesFromAssembly(Assembly.GetExecutingAssembly())
+                .Select(t => (t, pc: harmony.CreateClassProcessor(t)))
+                .Where(tuple => tuple.pc.HasPatchAttribute())
+                .ToArray();
 
-                try
-                {
-                    Logger.Log($"Loading {assFile}");
+            RunPatches(patchClasses.Where(tuple => tuple.pc.GetCategory() != "Experimental"));
 
-                    ass = Assembly.LoadFrom(assFile);
+            //foreach (var (t, pc) in patchClasses.Where(tuple => tuple.pc.GetCategory() != "Experimental"))
+            //{
+            //    try
+            //    {
+            //        Logger.Log($"Running patch class {t.Name}");
+            //        pc.Patch();
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Logger.Error($"Exception in patch class {t.Name}");
+            //        Logger.LogException(ex);
+            //    }
+            //}
 
-                    if (ass is null)
-                    {
-                        Logger.Log($"Failed to load assembly");
-                        continue;
-                    }
+            //foreach (var assFile in Directory.EnumerateFiles(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "*.patch.dll"))
+            //{
+            //    Assembly? ass = null;
 
-                    Logger.Log($"Running patches from {ass}");
+            //    try
+            //    {
+            //        Logger.Log($"Loading {assFile}");
 
-                    harmony.PatchAll(ass);
-                }
-                catch (HarmonyException hex)
-                {
-                    Logger.Error($"Exception while executing patch from {ass}");
-                    Logger.LogException(hex);
-                }
-                catch (Exception ex)
-                {
-                    Logger.Error($"Exception while trying to run patches from {ass?.ToString() ?? assFile.ToString()}");
-                    Logger.LogException(ex);
-                }
-            }
+            //        ass = Assembly.LoadFrom(assFile);
+
+            //        if (ass is null)
+            //        {
+            //            Logger.Log($"Failed to load assembly");
+            //            continue;
+            //        }
+
+            //        Logger.Log($"Running patches from {ass}");
+
+            //        harmony.PatchAll(ass);
+            //    }
+            //    catch (HarmonyException hex)
+            //    {
+            //        Logger.Error($"Exception while executing patch from {ass}");
+            //        Logger.LogException(hex);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Logger.Error($"Exception while trying to run patches from {ass?.ToString() ?? assFile.ToString()}");
+            //        Logger.LogException(ex);
+            //    }
+            //}
 
 #if DEBUG
             Logger.Log("Running experimental patches");
             try
             {
-                harmony.PatchAll();
+                RunPatches(patchClasses.Where(tuple => tuple.pc.GetCategory() == "Experimental"));
+                //harmony.PatchCategory("Experimental");
+                //harmony.PatchAll();
             }
             catch(Exception ex)
             {
