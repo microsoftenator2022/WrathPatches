31b93b6e88febfd51d6c55d610e487081cf852e3
Aaron Maslen <aaron.maslen@gmail.com>
2023-11-25 15:27:41 +1100

    Add Harmony version check. TODO: proper settings

---
+++ WrathPatches/CheckHarmonyVersion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityModManagerNet;
+
+namespace WrathPatches
+{
+    static partial class Main
+    {
+        static bool CheckHarmonyVersion(Func<Version, bool> shouldReplace)
+        {
+            var harmonyAss = AppDomain.CurrentDomain.GetAssemblies()
+                .FirstOrDefault(ass => ass.GetName().Name == "0Harmony");
+            var harmonyPath = harmonyAss.Location;
+
+            var harmonyVersion = harmonyAss.GetName().Version;
+            Logger.Log($"Harmony version: {harmonyVersion}");
+
+            var ummAss = AppDomain.CurrentDomain.GetAssemblies()
+                .FirstOrDefault(ass => ass.GetName().Name == "UnityModManager");
+
+            var ummHarmonyPath = Path.Combine(Path.GetDirectoryName(ummAss.Location), "0Harmony.dll");
+
+            var ummHarmonyVersion = Version.Parse(FileVersionInfo.GetVersionInfo(ummHarmonyPath).FileVersion);
+            Logger.Log($"UMM Harmony version: {ummHarmonyVersion}");
+
+            if (harmonyVersion < ummHarmonyVersion)
+            {
+                Logger.Warning($"Harmony version {harmonyVersion} is lower than UMM Harmony version {ummHarmonyVersion}");
+
+                if (shouldReplace(harmonyVersion))
+                {
+                    Logger.Warning($"Copying {ummHarmonyPath} -> {harmonyPath}. Please restart the game.");
+
+                    File.Copy(harmonyPath, Path.Combine(Path.GetDirectoryName(harmonyPath), $"0Harmony.{harmonyVersion}.old"));
+                    File.Copy(ummHarmonyPath, harmonyPath, true);
+
+                    typeof(UnityModManager.UI).GetMethod("Load", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, new object[0]);
+
+                    UnityModManager.UI.Instance.ToggleWindow(true);
+
+                    ModEntry!.Info.DisplayName = $"{ModEntry!.Info.DisplayName} - RESTART REQUIRED";
+
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- WrathPatches/Main.cs
+++ WrathPatches/Main.cs
@@ -10,7 +10,7 @@ using UnityModManagerNet;
 
 namespace WrathPatches
 {
-    public static class Main
+    static partial class Main
     {
         private static UnityModManager.ModEntry? ModEntry;
 
@@ -37,6 +37,22 @@ namespace WrathPatches
         {
             ModEntry = modEntry;
 
+            static bool replaceHarmonyVersion(Version harmonyVersion)
+            {
+                var ignoreFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "IgnoreHarmonyVersion.txt");
+                if (File.Exists(ignoreFilePath))
+                {
+                    var text = File.ReadAllText(ignoreFilePath).Trim();
+
+                    return  !Version.TryParse(text, out var ignoreVersion) || ignoreVersion <= harmonyVersion;
+                }
+
+                return true;
+            }
+
+            if (!CheckHarmonyVersion(replaceHarmonyVersion))
+                return false;
+
             var harmony = new Harmony(modEntry.Info.Id);
 
             var patchClasses = AccessTools.GetTypesFromAssembly(Assembly.GetExecutingAssembly())
@@ -46,59 +62,11 @@ namespace WrathPatches
 
             RunPatches(patchClasses.Where(tuple => tuple.pc.GetCategory() != "Experimental"));
 
-            //foreach (var (t, pc) in patchClasses.Where(tuple => tuple.pc.GetCategory() != "Experimental"))
-            //{
-            //    try
-            //    {
-            //        Logger.Log($"Running patch class {t.Name}");
-            //        pc.Patch();
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        Logger.Error($"Exception in patch class {t.Name}");
-            //        Logger.LogException(ex);
-            //    }
-            //}
-
-            //foreach (var assFile in Directory.EnumerateFiles(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "*.patch.dll"))
-            //{
-            //    Assembly? ass = null;
-
-            //    try
-            //    {
-            //        Logger.Log($"Loading {assFile}");
-
-            //        ass = Assembly.LoadFrom(assFile);
-
-            //        if (ass is null)
-            //        {
-            //            Logger.Log($"Failed to load assembly");
-            //            continue;
-            //        }
-
-            //        Logger.Log($"Running patches from {ass}");
-
-            //        harmony.PatchAll(ass);
-            //    }
-            //    catch (HarmonyException hex)
-            //    {
-            //        Logger.Error($"Exception while executing patch from {ass}");
-            //        Logger.LogException(hex);
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        Logger.Error($"Exception while trying to run patches from {ass?.ToString() ?? assFile.ToString()}");
-            //        Logger.LogException(ex);
-            //    }
-            //}
-
 #if DEBUG
             Logger.Log("Running experimental patches");
             try
             {
                 RunPatches(patchClasses.Where(tuple => tuple.pc.GetCategory() == "Experimental"));
-                //harmony.PatchCategory("Experimental");
-                //harmony.PatchAll();
             }
             catch(Exception ex)
             {
