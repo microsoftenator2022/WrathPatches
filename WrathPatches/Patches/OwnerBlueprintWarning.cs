b209a22a62bd1b1c8268cbe7441d856b98ab3158
Aaron Maslen <aaron.maslen@gmail.com>
2024-07-30 19:31:09 +1000

    v1.9.1
    Make OwnerBlueprint message less frequent (only print for component types that may be affected) and increase severity to Error

--- WrathPatches/Patches/OwnerBlueprintWarning.cs
+++ WrathPatches/Patches/OwnerBlueprintWarning.cs
@@ -6,15 +6,76 @@ using System.Threading.Tasks;
 
 using HarmonyLib;
 
+using Kingmaker.AreaLogic.Capital;
+using Kingmaker.AreaLogic.Etudes;
+using Kingmaker.Armies.Components;
+using Kingmaker.Armies.TacticalCombat.LeaderSkills;
 using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes.Experience;
+using Kingmaker.Blueprints.Classes.Prerequisites;
 using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.Craft;
+using Kingmaker.Designers.EventConditionActionSystem.Events;
+using Kingmaker.Designers.Mechanics.Buffs;
+using Kingmaker.Dungeon.FactLogic;
+using Kingmaker.Kingdom.Settlements.BuildingComponents;
+using Kingmaker.Tutorial;
+using Kingmaker.UnitLogic.Abilities.Components;
+using Kingmaker.UnitLogic.Abilities.Components.Base;
+using Kingmaker.UnitLogic.Abilities.Components.TargetCheckers;
+using Kingmaker.UnitLogic.ActivatableAbilities;
+using Kingmaker.UnitLogic.FactLogic;
+using Kingmaker.UnitLogic.Mechanics.Components;
+using Kingmaker.UnitLogic.Mechanics.Properties;
 
 namespace WrathPatches.Patches;
 
 [WrathPatch("Non-matching BlueprintComponent.OwnerBlueprint warning")]
 [HarmonyPatch]
-internal class OwnerBlueprintWarning
+public class OwnerBlueprintWarning
 {
+    /// <summary>
+    /// List of BlueprintComponent types that use OwnerBlueprint
+    /// </summary>
+    public static readonly IEnumerable<Type> ErrorComponentTypes =
+    [
+        typeof(CapitalCompanionLogic),
+        typeof(EtudeBracketMusic),
+        typeof(EtudeBracketSetCompanionPosition),
+        //typeof(EtudeBracketTrigger<TData>),
+        typeof(ArmyUnitComponent),
+        typeof(LeaderPercentAttributeBonus),
+        typeof(MaxArmySquadsBonusLeaderComponent),
+        typeof(SquadsActionOnTacticalCombatStart),
+        typeof(Experience),
+        typeof(PrerequisiteArchetypeLevel),
+        typeof(PrerequisiteClassLevel),
+        typeof(PrerequisiteFeature),
+        typeof(CraftInfoComponent),
+        typeof(EvaluatedUnitCombatTrigger),
+        typeof(ControlledProjectileHolder),
+        typeof(DungeonAddLootToVendor),
+        typeof(BuildingUpgradeBonus),
+        typeof(TutorialPage),
+        typeof(AbilityCustomDimensionDoor),
+        typeof(AbilityDeliverProjectileOnGrid),
+        typeof(AbilityIsBomb),
+        typeof(AbilityDeliverEffect),
+        typeof(MarkUsableWhileCan),
+        typeof(ActivatableAbilitySet),
+        typeof(ActivatableAbilitySetItem),
+        typeof(AddAbilityUseTrigger),
+        typeof(AddFeaturesFromSelectionToDescription),
+        typeof(AddTriggerOnActivationChanged),
+        typeof(AddVendorItems),
+        typeof(NenioSpecialPolymorphWhileEtudePlaying),
+        typeof(ChangeSpellElementalDamage),
+        typeof(ContextCalculateAbilityParams),
+        typeof(ContextRankConfig),
+        typeof(ContextSetAbilityParams),
+        typeof(UnitPropertyComponent)
+    ];
+
     [HarmonyPatch(typeof(BlueprintsCache), nameof(BlueprintsCache.Load))]
     [HarmonyPostfix]
     static void Postfix(SimpleBlueprint __result, BlueprintGuid guid)
@@ -26,8 +87,19 @@ internal class OwnerBlueprintWarning
         {
             if (c.OwnerBlueprint != blueprint)
             {
-                Main.Logger.Warning($"In blueprint {guid} \"{blueprint.name}\": " +
-                    $"Non-matching OwnerBlueprint {c.OwnerBlueprint?.ToString() ?? "NULL"} on {c.GetType()} \"{c.name}\"");
+                if (ErrorComponentTypes.Any(t => t.IsAssignableFrom(c.GetType())))
+                {
+                    Main.Logger.Error($"In blueprint {guid} \"{blueprint.name}\": " +
+                        $"Non-matching OwnerBlueprint {c.OwnerBlueprint?.ToString() ?? "NULL"} on {c.GetType()} \"{c.name}\". " +
+                        $"THIS COMPONENT MAY NOT WORK AS EXPECTED!");
+                }
+#if DEBUG
+                else
+                {
+                    Main.Logger.Warning($"In blueprint {guid} \"{blueprint.name}\": " +
+                        $"Non-matching OwnerBlueprint {c.OwnerBlueprint?.ToString() ?? "NULL"} on {c.GetType()} \"{c.name}\"");
+                }
+#endif
             }
         }
     }
