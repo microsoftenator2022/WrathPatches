bc67a343603594df6d54681dd15fb913b841cf23
Aaron Maslen <aaron.maslen@gmail.com>
2024-08-16 11:35:48 +1000

    v1.10.0
    Enable an owlcat dev mode hotkey (Ctrl+Shift+Alt+F) to open gamelogfull.txt and patch to also open player.log

--- WrathPatches/Main.cs
+++ WrathPatches/Main.cs
@@ -3,151 +3,160 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 
 using HarmonyLib;
+
+using Kingmaker;
+using Kingmaker.Cheats;
+using Kingmaker.Designers.Mechanics.Facts;
+using Kingmaker.RuleSystem.Rules.Damage;
 
 using UnityEngine;
 
 using UnityModManagerNet;
 
-namespace WrathPatches
+using WrathPatches.Patches;
+
+namespace WrathPatches;
+
+static partial class Main
 {
-    static partial class Main
+    static void OnGUI(UnityModManager.ModEntry _)
     {
-        static void OnGUI(UnityModManager.ModEntry _)
+        GUILayout.BeginHorizontal();
         {
-            GUILayout.BeginHorizontal();
+            GUILayout.BeginVertical();
             {
-                GUILayout.BeginVertical();
+                GUILayout.Label("Patches Status");
+
+                var font = GUI.skin.label.font;
+
+                foreach (var (t, pc) in PatchClasses.Value)
                 {
-                    GUILayout.Label("Patches Status");
+                    var name = t.Name;
 
-                    var font = GUI.skin.label.font;
+                     AppliedPatches.TryGetValue(t, out var applied);
 
-                    foreach (var (t, pc) in PatchClasses.Value)
-                    {
-                        var name = t.Name;
+                    if (t.GetCustomAttribute<WrathPatchAttribute>() is { } attr)
+                        name = attr.Name;
 
-                         AppliedPatches.TryGetValue(t, out var applied);
+                    if (IsExperimental(pc))
+                        name = $"(Experimental) {name}";
 
-                        if (t.GetCustomAttribute<WrathPatchAttribute>() is { } attr)
-                            name = attr.Name;
-
-                        if (IsExperimental(pc))
-                            name = $"(Experimental) {name}";
-
-                        GUILayout.Toggle(applied is true, name);
-                    }
+                    GUILayout.Toggle(applied is true, name);
                 }
-                GUILayout.EndVertical();
+            }
+            GUILayout.EndVertical();
 
-                GUILayout.BeginVertical();
+            GUILayout.BeginVertical();
+            {
+                static bool IsVersionMisMatch() => HarmonyVersion < UmmHarmonyVersion;
+
+                GUILayout.BeginHorizontal();
                 {
-                    static bool IsVersionMisMatch() => HarmonyVersion < UmmHarmonyVersion;
+                    GUILayout.Label($"Harmony version: {HarmonyVersion}");
 
-                    GUILayout.BeginHorizontal();
+                    if (IsVersionMisMatch() && ModEntry!.Active)
                     {
-                        GUILayout.Label($"Harmony version: {HarmonyVersion}");
-
-                        if (IsVersionMisMatch() && ModEntry!.Active)
+                        if (GUILayout.Button("Update"))
                         {
-                            if (GUILayout.Button("Update"))
-                            {
-                                ModEntry!.OnToggle = (_, value) => !value;
-                                ModEntry!.Active = false;
-                                ModEntry!.Info.DisplayName = $"{ModEntry!.Info.DisplayName} - RESTART REQUIRED";
+                            ModEntry!.OnToggle = (_, value) => !value;
+                            ModEntry!.Active = false;
+                            ModEntry!.Info.DisplayName = $"{ModEntry!.Info.DisplayName} - RESTART REQUIRED";
 
-                                ReplaceHarmony(GetHarmonyAss().Location, UmmHarmonyPath);
-                            }
+                            ReplaceHarmony(GetHarmonyAss().Location, UmmHarmonyPath);
                         }
                     }
-                    GUILayout.EndHorizontal();
-
-                    GUILayout.Label($"UMM Harmony Version: {UmmHarmonyVersion}");
                 }
-                GUILayout.EndVertical();
+                GUILayout.EndHorizontal();
+
+                GUILayout.Label($"UMM Harmony Version: {UmmHarmonyVersion}");
             }
-            GUILayout.EndHorizontal();
+            GUILayout.EndVertical();
         }
+        GUILayout.EndHorizontal();
+    }
 
-        private static UnityModManager.ModEntry? ModEntry;
+    private static UnityModManager.ModEntry? ModEntry;
 
-        public static UnityModManager.ModEntry.ModLogger Logger => ModEntry!.Logger;
+    public static UnityModManager.ModEntry.ModLogger Logger => ModEntry!.Logger;
 
-        static Harmony HarmonyInstance = null!;
+    static Harmony HarmonyInstance = null!;
 
-        static readonly Lazy<(Type t, PatchClassProcessor pc)[]> PatchClasses = new(() =>
-            AccessTools.GetTypesFromAssembly(Assembly.GetExecutingAssembly())
-                .Select(t => (t, pc: HarmonyInstance!.CreateClassProcessor(t)))
-                .Where(tuple => tuple.pc.HasPatchAttribute())
-                .ToArray());
+    static readonly Lazy<(Type t, PatchClassProcessor pc)[]> PatchClasses = new(() =>
+        AccessTools.GetTypesFromAssembly(Assembly.GetExecutingAssembly())
+            .Select(t => (t, pc: HarmonyInstance!.CreateClassProcessor(t)))
+            .Where(tuple => tuple.pc.HasPatchAttribute())
+            .ToArray());
 
-        static readonly Dictionary<Type, bool?> AppliedPatches = new();
+    static readonly Dictionary<Type, bool?> AppliedPatches = new();
 
-        static bool IsExperimental(PatchClassProcessor pc) => pc.GetCategory() == "Experimental";
+    static bool IsExperimental(PatchClassProcessor pc) => pc.GetCategory() == "Experimental";
 
-        static void RunPatches(IEnumerable<(Type, PatchClassProcessor)> typesAndPatches)
+    static void RunPatches(IEnumerable<(Type, PatchClassProcessor)> typesAndPatches)
+    {
+        foreach (var (t, pc) in typesAndPatches)
         {
-            foreach (var (t, pc) in typesAndPatches)
-            {
-                try
-                {
-                    Logger.Log($"Running patch class {t.Name}");
-                    pc.Patch();
-
-                    AppliedPatches[t] = true;
-                }
-                catch (Exception ex)
-                {
-                    Logger.Error($"Exception in patch class {t.Name}");
-                    Logger.LogException(ex);
-
-                    AppliedPatches[t] = false;
-                }
-            }
-        }
-
-        static bool Load(UnityModManager.ModEntry modEntry)
-        {
-            ModEntry = modEntry;
-
-            ModEntry.OnGUI = OnGUI;
-
-            ModEntry.OnToggle = (_, value) => value;
-
-            //static bool replaceHarmonyVersion(Version harmonyVersion)
-            //{
-            //    var ignoreFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "IgnoreHarmonyVersion.txt");
-            //    if (File.Exists(ignoreFilePath))
-            //    {
-            //        var text = File.ReadAllText(ignoreFilePath).Trim();
-
-            //        return  !Version.TryParse(text, out var ignoreVersion) || ignoreVersion <= harmonyVersion;
-            //    }
-
-            //    return true;
-            //}
-
-            //if (!CheckHarmonyVersion(replaceHarmonyVersion))
-            //    return false;
-
-            HarmonyInstance = new Harmony(modEntry.Info.Id);
-
-            RunPatches(PatchClasses.Value.Where(tuple => !IsExperimental(tuple.pc)));
-
-#if DEBUG
-            Logger.Log("Running experimental patches");
             try
             {
-                RunPatches(PatchClasses.Value.Where(tuple => IsExperimental(tuple.pc)));
+                Logger.Log($"Running patch class {t.Name}");
+                pc.Patch();
+
+                AppliedPatches[t] = true;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
+                Logger.Error($"Exception in patch class {t.Name}");
                 Logger.LogException(ex);
+
+                AppliedPatches[t] = false;
             }
+        }
+    }
+
+    internal static bool Load(UnityModManager.ModEntry modEntry)
+    {
+        ModEntry = modEntry;
+
+        ModEntry.OnGUI = OnGUI;
+
+        ModEntry.OnToggle = (_, value) => value;
+
+        //static bool replaceHarmonyVersion(Version harmonyVersion)
+        //{
+        //    var ignoreFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "IgnoreHarmonyVersion.txt");
+        //    if (File.Exists(ignoreFilePath))
+        //    {
+        //        var text = File.ReadAllText(ignoreFilePath).Trim();
+
+        //        return  !Version.TryParse(text, out var ignoreVersion) || ignoreVersion <= harmonyVersion;
+        //    }
+
+        //    return true;
+        //}
+
+        //if (!CheckHarmonyVersion(replaceHarmonyVersion))
+        //    return false;
+
+        HarmonyInstance = new Harmony(modEntry.Info.Id);
+
+        RunPatches(PatchClasses.Value.Where(tuple => !IsExperimental(tuple.pc)));
+
+#if DEBUG
+        Logger.Log("Running experimental patches");
+        try
+        {
+            RunPatches(PatchClasses.Value.Where(tuple => IsExperimental(tuple.pc)));
+        }
+        catch(Exception ex)
+        {
+            Logger.LogException(ex);
+        }
 #endif
 
-            return true;
-        }
+        LogsHotkey.EnableHotKey();
+
+        return true;
     }
 }
---
+++ WrathPatches/Patches/LogsHotkey.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+
+using Kingmaker;
+using Kingmaker.Cheats;
+
+using UnityEngine;
+
+namespace WrathPatches.Patches;
+
+[WrathPatch("OpenGameLogFull opens Player.log too")]
+[HarmonyPatch(typeof(CheatsDebug), nameof(CheatsDebug.OpenGameLogFull), [typeof(string)])]
+internal static class LogsHotkey
+{
+    public static void EnableHotKey()
+    {
+
+        Main.Logger.Log("Enabling GameLogFull keybind");
+
+        Game.Instance.Keyboard.Bind("OpenGameLogFull", CheatsDebug.OpenGameLogFull);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch]
+    static void OpenGameLogFull_Postfix()
+    {
+        try
+        {
+            Application.OpenURL(Path.Combine(Application.persistentDataPath, "Player.log"));
+        }
+        catch
+        {
+        }
+    }
+}
