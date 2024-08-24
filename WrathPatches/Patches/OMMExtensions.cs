61debc2abc844d1222cb694ee4737b3f2f2954db
Aaron Maslen <aaron.maslen@gmail.com>
2024-08-24 23:58:39 +1000

    Owlcat mod extensions: - Allow to have UMM mods as dependencies - Compare dependency versions like UMM (previously version of dependency had to be exact match) - Kurufinves' OMM Enhancer: patch components individually without having to patch the entire array

---
+++ WrathPatches/Patches/OMMExtensions.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+
+using Kingmaker.Modding;
+
+using Newtonsoft.Json;
+
+using UnityModManagerNet;
+
+using WrathPatches.TranspilerUtil;
+
+namespace WrathPatches.Patches;
+
+[HarmonyPatchCategory("Experimental")]
+[WrathPatch("OwlcatModificationManager Assemblies load patch")]
+[HarmonyPatch]
+internal static class LoadAssembliesFromList
+{
+    [HarmonyPatch(typeof(OwlcatModification), nameof(OwlcatModification.GetFilesFromDirectory))]
+    [HarmonyPostfix]
+    static IEnumerable<string> GetFilesFromDirectory_Postfix(IEnumerable<string> result, string directory)
+    {
+        var dirName = new DirectoryInfo(directory).Name;
+
+        var isAssemblies = dirName.Equals("Assemblies", StringComparison.InvariantCultureIgnoreCase);
+
+        var assembliesListJson = Directory.EnumerateFiles(directory, "AssembliesList.json").FirstOrDefault();
+
+        var assembliesList = assembliesListJson is not null ? JsonConvert.DeserializeObject<AssembliesList>(File.ReadAllText(assembliesListJson)) : null;
+
+        foreach (var item in result)
+        {
+            if (!isAssemblies)
+            {
+                yield return item;
+                continue;
+            }
+
+            if (!Path.GetExtension(item).Equals("dll", StringComparison.InvariantCultureIgnoreCase))
+                continue;
+
+            if (assembliesList is null)
+            {
+                yield return item;
+                continue;
+            }
+
+            if (assembliesList.AssemblyNames.Any(assName => Path.GetFileNameWithoutExtension(item).Equals(assName, StringComparison.InvariantCultureIgnoreCase)))
+                yield return item;
+        }
+    }
+
+    class AssembliesList
+    {
+        public List<string> AssemblyNames = [];
+    }
+}
+
+[WrathPatch("OwlcatModificationManager: UMM mods as dependencies for OMM mods")]
+[HarmonyPatch(typeof(OwlcatModificationsManager))]
+internal static class UMMDependency
+{
+
+    static IEnumerable<OwlcatModificationManifest> UMMMods
+    {
+        get
+        {
+            foreach (var modInfo in UnityModManager.modEntries.Select(me => me.Info))
+            {
+                yield return new()
+                {
+                    UniqueName = modInfo.Id,
+                    DisplayName = modInfo.DisplayName,
+                    Version = modInfo.Version
+                };
+            }
+        }
+    }
+
+    static OwlcatModificationManifest? TryGetUMMMod(OwlcatModificationManifest.Dependency dependency) =>
+        UMMMods.FirstOrDefault(m => m.UniqueName == dependency.Name);
+
+    static FieldInfo DependencyLocalValue =>
+        typeof(OwlcatModificationsManager).GetNestedTypes(AccessTools.all)
+            .Select(t => t.GetField("dependency", AccessTools.all))
+            .SkipIfNull()
+            .Single(fi => fi.FieldType == typeof(OwlcatModificationManifest.Dependency));
+
+    [HarmonyPatch(nameof(OwlcatModificationsManager.CheckDependencies))]
+    [HarmonyTranspiler]
+    static IEnumerable<CodeInstruction> CheckDependencies_Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var match = instructions.FindInstructionsIndexed(
+        [
+            ci => ci.opcode == OpCodes.Stloc_S && ci.operand is LocalBuilder { LocalIndex: 5 },
+            ci => ci.opcode == OpCodes.Ldloc_S && ci.operand is LocalBuilder { LocalIndex: 5 },
+            ci => ci.opcode == OpCodes.Brtrue_S
+        ]).ToArray();
+
+        if (match.Length != 3)
+            throw new Exception($"Could not find instructions to patch");
+
+        var dependencyFound = match[2].instruction.operand;
+
+        var iList = instructions.ToList();
+
+        var getUMMDependency = new CodeInstruction[]
+        {
+            new(OpCodes.Ldloc_S, 4),
+            new(OpCodes.Ldfld, DependencyLocalValue),
+            CodeInstruction.Call((OwlcatModificationManifest.Dependency dependency) => TryGetUMMMod(dependency)),
+            new(OpCodes.Dup),
+            new(OpCodes.Stloc_S, 5),
+            new(OpCodes.Brtrue_S, dependencyFound)
+        };
+
+        iList.InsertRange(match[2].index + 1, getUMMDependency);
+
+        return iList;
+    }
+}
+
+[WrathPatch("OwlcatModificationManager: Compare OMM mod versions like UMM")]
+[HarmonyPatch(typeof(OwlcatModificationsManager))]
+static class OMMVersionCheck
+{
+    // Return true if the version is too low
+    static bool VersionCheck(string thisVersionString, string otherVersionString)
+    {
+        var thisVersion = UnityModManager.ParseVersion(thisVersionString);
+        var otherVersion = UnityModManager.ParseVersion(otherVersionString);
+
+        return otherVersion < thisVersion;
+    }
+
+    [HarmonyPatch(nameof(OwlcatModificationsManager.CheckDependencies))]
+    [HarmonyTranspiler]
+    static IEnumerable<CodeInstruction> CheckDependencies_Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+#if DEBUG
+        var patched = false;
+#endif
+
+        foreach (var i in instructions)
+        {
+            if (i.Calls(AccessTools.Method(typeof(string), "op_Inequality", [typeof(string), typeof(string)])))
+            {
+#if DEBUG
+                patched = true;
+#endif
+
+                yield return CodeInstruction.Call((string a, string b) => VersionCheck(a, b)).WithLabels(i.labels);
+                continue;
+            }
+
+            yield return i;
+        }
+
+#if DEBUG
+        if (!patched)
+            throw new Exception("Could not find instructions to patch");
+#endif
+    }
+}
---
+++ WrathPatches/Patches/TheEnhance.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+using HarmonyLib;
+using Kingmaker;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.EntitySystem.Persistence.JsonUtility;
+using Kingmaker.Modding;
+using Kingmaker.UnitLogic.Mechanics.Actions;
+using Kingmaker.Utility;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using WrathPatches;
+
+namespace OwlModPatcherEnhancer
+{
+    [WrathPatch("Owlmod Enhancer")]
+    [HarmonyPatch]
+    public static class TheEnhance
+    {
+        //static JsonSerializerSettings settings = new JsonSerializerSettings()
+        //{
+
+        //    TypeNameHandling = TypeNameHandling.Auto,
+        //    NullValueHandling = NullValueHandling.Ignore,
+        //    DefaultValueHandling = DefaultValueHandling.Ignore,
+        //    PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+        //    ReferenceLoopHandling = ReferenceLoopHandling.Error,
+        //    Formatting = Formatting.Indented,
+        //    Binder = new GuidClassBinder(),
+        //    Converters = DefaultJsonSettings.CommonConverters.ToList<JsonConverter>(),
+        //    ContractResolver = new OptInContractResolver(),
+
+        //};
+
+        static BlueprintJsonWrapper WrapperInstance = new();
+
+        static JsonMergeSettings MergeSettings = default!;
+        [HarmonyPrepare]
+        static bool Prepare()
+        {
+            MergeSettings ??= (JsonMergeSettings)AccessTools.DeclaredField(typeof(OwlcatModificationBlueprintPatcher), "MergeSettings").GetValue(null);
+#if DEBUG
+            PFLog.Mods.Log($"OwlModPatcherEnhancer Prepare - MergeSettings is null? {MergeSettings == null}");
+#endif
+            return MergeSettings != null;
+        }
+
+
+        [HarmonyPatch(typeof(OwlcatModificationBlueprintPatcher), "ApplyPatchEntry", new Type[] { typeof(JObject), typeof(JObject) })]
+        [HarmonyPostfix]
+        public static void PostfixComponent(JObject jsonBlueprint, JObject patchEntry)
+        {
+            if (jsonBlueprint["Components"] is not JArray components)
+                return;
+
+            if (patchEntry["ComponentPatches"] is not JArray componentPatches)
+                return;
+
+            using IEnumerator<JToken> enumerator = componentPatches.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                if (enumerator.Current is not JObject currentComponentPatch)
+                    continue;
+                var componentName = currentComponentPatch["Name"].ToString();
+
+                try
+                {
+                    if (componentName.IsNullOrEmpty())
+                    {
+                        PFLog.Mods.Warning($"OwlModPatcherEnhance - a component patch with empty Name");
+                        continue;
+                    }
+                    JObject? componentToPatch = null;
+
+                    using (var comps = components.GetEnumerator())
+                    {
+                        while (comps.MoveNext())
+                        {
+                            var name = (comps.Current as JObject)?["name"]?.ToString();
+                            {
+                                if (name.IsNullOrEmpty())
+                                    PFLog.Mods.Warning("OwlModPatcherEnhance Found a component with null or empty name: \n" + (comps.Current as JObject));
+                            }
+                            if (name != componentName)
+                                continue;
+                            componentToPatch = comps.Current as JObject;
+                        }
+                    }
+
+                    if (componentToPatch == null)
+                    {
+                        PFLog.Mods.Warning($"Failed to find component with the name '{componentName}' when patching blueprint {jsonBlueprint["name"]} (guid {jsonBlueprint["AssetGuid"]}");
+                    }
+                    var patchString = currentComponentPatch["Data"];
+                    componentToPatch!.Merge(patchString, MergeSettings);
+                }
+                catch (Exception ex)
+                {
+                    PFLog.Mods.Exception(ex, $"Error on patching blueprint {jsonBlueprint["name"]} (guid {jsonBlueprint["AssetGuid"]}) with component {componentName}");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(OwlcatModificationBlueprintPatcher), nameof(OwlcatModificationBlueprintPatcher.ApplyPatch), new Type[] { typeof(SimpleBlueprint), typeof(JObject)})]
+        [HarmonyPostfix]
+        public static void PostfixElement(SimpleBlueprint __result, JObject patch)
+        {
+            if (__result is not BlueprintScriptableObject blueprint)
+            {
+                return;
+            }
+
+            if (patch["ElementsPatches"] is not JArray elementPatches)
+            {
+                return;
+            }
+
+            using IEnumerator<JToken> enumerator = elementPatches.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                var currentPatch = (JObject)enumerator.Current;
+                var elementName = currentPatch["Name"].ToString();
+                if (elementName.IsNullOrEmpty())
+                {
+                    PFLog.Mods.Warning($"OwlModPatcherEnhancer found a patch with empty element name when patching blueprint {blueprint.name} (guid {blueprint.AssetGuid})");
+                    continue;
+                }
+                var element = blueprint.ElementsArray.FirstOrDefault(el => el.name == elementName);
+                if (element == null)
+                {
+                    PFLog.Mods.Warning($"OwlModPatcherEnhancer failed to find an element with name {elementName} when patching blueprint {blueprint.name} (guid {blueprint.AssetGuid})");
+                    continue;
+                }
+                try
+                {
+
+                    var elementPatch = currentPatch["Data"].ToString();
+                    WrapperInstance.Data = blueprint;
+                    Json.BlueprintBeingRead = WrapperInstance;
+                    JsonConvert.PopulateObject(elementPatch, element);
+                    Json.BlueprintBeingRead = null;
+                    blueprint.RemoveFromElementsList(element);
+                }
+                catch (Exception ex)
+                {
+                    PFLog.Mods.Exception(ex, $"Exception when using OwlModPatcherEnhancer to patch element {elementName} on blueprint {blueprint.name} (guid {blueprint.AssetGuid}");
+                }
+            }
+
+        }
+
+    }
+}
