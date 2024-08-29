f8ba2e062d195468f4a1fb9054648515ab3d467a
Aaron Maslen <aaron.maslen@gmail.com>
2024-08-29 23:36:38 +1000

    Remove obsolete experiments

---
+++ WrathPatches/Patches/Experimental/DisableHands.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Kingmaker.UnitLogic;
+
+namespace WrathPatches.Patches.Experimental;
+
+[Obsolete]
+internal class DisableHands : UnitFactComponentDelegate
+{ }
