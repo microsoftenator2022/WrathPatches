e706e5468ea569080531a851f9c9588705caa67d
Aaron Maslen <aaron.maslen@gmail.com>
2025-06-16 11:37:48 +1000

    Experimental fix for bad interaction between toybox threaded loader and blueprint patch enhancer

---
+++ WrathPatches/Patches/Experimental/MakeBlueprintBeingReadThreadStatic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using HarmonyLib;
+
+using Kingmaker.Blueprints.JsonSystem;
+
+namespace WrathPatches.Patches;
+
+[HarmonyPatchCategory("Experimental")]
+[WrathPatch("Make BlueprintBeingRead ThreadStatic")]
+[HarmonyPatch(typeof(Json), nameof(Json.BlueprintBeingRead))]
+internal static class MakeBlueprintBeingReadThreadStatic
+{
+    [ThreadStatic]
+    static BlueprintJsonWrapper? blueprintBeingRead;
+
+    [HarmonyPatch(MethodType.Setter)]
+    [HarmonyPostfix]
+    static void SetterPatch(BlueprintJsonWrapper value) =>
+        blueprintBeingRead = value;
+
+    [HarmonyPatch(MethodType.Getter)]
+    [HarmonyPostfix]
+    static void GetterPatch(ref BlueprintJsonWrapper? __result) => 
+        __result = blueprintBeingRead;
+}
