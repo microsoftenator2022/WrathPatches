74829ee1bcff349e2b70fad0bf58a12f6ccfa845
Aaron Maslen <aaron.maslen@gmail.com>
2025-07-30 12:40:27 +1000

    Fix for units incorrectly excluded from surprise rounds

---
+++ WrathPatches/Patches/ActingInSurpriseRoundFix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+using HarmonyLib;
+
+using TurnBased.Controllers;
+
+namespace WrathPatches.Patches;
+
+[WrathPatch("Fix surprise round turns")]
+[HarmonyPatch(typeof(CombatController), nameof(CombatController.HandleCombatStart))]
+internal class ActingInSurpriseRoundFix
+{
+	static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+	{
+		foreach (var i in instructions)
+		{
+			if (i.opcode == OpCodes.Call &&
+				(MethodInfo)i.operand == AccessTools.PropertyGetter(typeof(TimeSpan), nameof(TimeSpan.Seconds)))
+				i.operand = AccessTools.PropertyGetter(typeof(TimeSpan), nameof(TimeSpan.TotalSeconds));
+			yield return i;
+		}
+	}
+}
